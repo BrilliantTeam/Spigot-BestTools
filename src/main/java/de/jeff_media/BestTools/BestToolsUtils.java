90972675d6f487aad2bf17bc0f905d8c6c54ec67
RICE0707 <master.end.9377@gmail.com>
2024-12-22 12:19:09 +0800

    註解掉 MINEABL，因為會覆蓋掉先前邏輯。

--- src/main/java/de/jeff_media/BestTools/BestToolsUtils.java
+++ src/main/java/de/jeff_media/BestTools/BestToolsUtils.java
@@ -555,13 +555,13 @@ public class BestToolsUtils {
         }
 
         // This is fairly expensive, but 2ms vs 8ms isn't a meaningful difference.
-        try {
-            tagToMap(Tag.MINEABLE_AXE, Tool.AXE);
-            tagToMap(Tag.MINEABLE_HOE, Tool.HOE);
-            tagToMap(Tag.MINEABLE_PICKAXE, Tool.PICKAXE);
-            tagToMap(Tag.MINEABLE_SHOVEL, Tool.SHOVEL);
-        } catch (NoSuchFieldError | NoClassDefFoundError ignored) {
-        }
+        //try {
+        //    tagToMap(Tag.MINEABLE_AXE, Tool.AXE);
+        //    tagToMap(Tag.MINEABLE_HOE, Tool.HOE);
+        //    tagToMap(Tag.MINEABLE_PICKAXE, Tool.PICKAXE);
+        //    tagToMap(Tag.MINEABLE_SHOVEL, Tool.SHOVEL);
+        //} catch (NoSuchFieldError | NoClassDefFoundError ignored) {
+        //}
 
 
         long endTime = System.nanoTime();
